8a334bd6aeab8b2bb1a93387b7ecd596a8e707e4
Julius Simtim <juliussimtim@gmail.com>
2026-07-20 20:22:20 -0700

    Made changes for frontend dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   async rewrites() {
     const fastApiParseTaskUrl = process.env.FASTAPI_PARSE_TASK_URL ?? "http://127.0.0.1:8000/parse-task";
     return [
